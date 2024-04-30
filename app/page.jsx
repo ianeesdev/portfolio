e092dccc7f5a2d2f145b1197570bec86ef89d52f
Muhammad Anees <aneese421@gmail.com>
2024-05-01 00:14:51 +0500

    Update global styles, add Hero component, and update button styles
    The diff includes updates to global CSS styles, adding a new Hero component in the home page, and updating button styles with new variants and sizes.

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <main>Homepage</main>;
+  return <main>
+    <Hero />
+  </main>;
 }
  
