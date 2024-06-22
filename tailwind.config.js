15a7834f8b981068fbc7eddbc6fafb99cba8fdde
Muhammad Anees <aneese421@gmail.com>
2024-06-22 18:00:14 +0500

    Added Work component to Home page and updated Tailwind config with new image URLs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   prefix: "",
   theme: {
@@ -78,16 +78,18 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
       backgroundImage: {
-        hero: 'url(/hero/hero-bg.png)',
-        hero_shape: 'url(/hero/shape-1.svg)',
-        hero_shape2_light: 'url(/hero/shape-2-light.svg)',
-        hero_shape2_dark: 'url(/hero/shape-2-dark.svg)',
-        about_shape_light: 'url(/about/shape-light.svg)',
-        about_shape_dark: 'url(/about/shape-dark.svg)',
-        dots_light: 'url(/dots-light.svg)',
-        dots_dark: 'url(/dots-dark.svg)',
-      }
+        hero: "url(/hero/hero-bg.png)",
+        hero_shape: "url(/hero/shape-1.svg)",
+        hero_shape2_light: "url(/hero/shape-2-light.svg)",
+        hero_shape2_dark: "url(/hero/shape-2-dark.svg)",
+        about_shape_light: "url(/about/shape-light.svg)",
+        about_shape_dark: "url(/about/shape-dark.svg)",
+        dots_light: "url(/dots-light.svg)",
+        dots_dark: "url(/dots-dark.svg)",
+        work_project_bg_light: "url(/work/project-bg-light.png)",
+        work_project_bg_dark: "url(/work/project-bg-dark.png)",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
