7948087709ec16dd514c6f94d22f897c962d19c3
Muhammad Anees <aneese421@gmail.com>
2024-06-23 21:22:24 +0500

    Updated globals.css with swiper bullets styles and modified ProjectCard.jsx to add buttons with links and icons

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -19,7 +19,20 @@ const ProjectCard = ({ project }) => {
             alt="project image"
           />
           {/* btns */}
-          
+          <div className="flex gap-x-4">
+            <Link
+              href={project.link}
+              className="bg-secondary size-[50px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-300"
+            >
+              <Link2Icon className="text-white" />
+            </Link>
+            <Link
+              href={project.github}
+              className="bg-secondary size-[50px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-300"
+            >
+              <Github className="text-white" />
+            </Link>
+          </div>
         </div>
       </CardHeader>
       <div className="h-full px-8 py-6">
