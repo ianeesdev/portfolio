55df0a5752d702c30f381964a8291d1adb4be0bd
Muhammad Anees <aneese421@gmail.com>
2024-07-07 21:51:06 +0500

    Updated Hero component to use TypewriterEffect

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Download, Send } from "lucide-react";
@@ -13,23 +15,32 @@ import DevImg from "./DevImg";
 import Badge from "./Badge";
 import Socials from "./Socials";
 
+import TypewriterComponent from "typewriter-effect";
+
 const Hero = () => {
   return (
-    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
+    <section className="py-12 xl:py-24 h-[88vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
       <div className="container mx-auto">
         <div className="flex justify-between gap-x-8">
           {/* Text */}
           <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
             <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
-              Web Developer
+              <TypewriterComponent
+                options={{
+                  strings: ["Web Developer", "MERN Stack", "NEXT JS"],
+                  autoStart: true,
+                  loop: true,
+                  delay: 70,
+                  deleteSpeed: 20,
+                }}
+              />
             </div>
             <h1 className="h1 xl:text-[68px] mb-4">
               Hello, My name is Muhammad Anees
             </h1>
             <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
-              A skilled MERN stack developer passionate about coding, with a
-              solid foundation in web technologies and a dedication to
-              continuous learning.
+              A skilled MERN stack developer with a solid foundation in web
+              technologies and a dedication to continuous learning.
             </p>
             {/* buttons */}
             <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
